093f93c02217c0c6b62fdb54a22774122a3fe4b1
Théophile JR <theophilejeromerocher44@gmail.com>
2024-11-04 15:42:41 +0100

    refactor(Architecture): Remove liskvork build files
    We will keep only the bin and the config.ini

--- liskvork-0.4.1-src/.zig-cache/c/253120d618332c007018bd3cdaa2cac0/options.zig
+++
@@ -1,3 +0,0 @@
-pub const version: []const u8 = "0.0.0-dev";
-pub const bin_name: []const u8 = "liskvork";
-pub const use_system_allocator: bool = false;
--- liskvork-0.4.1-src/pbrain-player2.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-while True:
-    a = str(input(''))
-    print("name=\"SomeBrain\", version=\"1.0\", author=\"Mathieu\", country=\"FR\"")
-    print("OK")
