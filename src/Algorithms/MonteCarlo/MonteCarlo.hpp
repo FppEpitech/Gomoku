93ac35d8e9639c3b3fb1730610b25cd77f62cabf
Théophile JR <theophilejeromerocher44@gmail.com>
2024-11-12 20:09:39 +0100

    feat: base functions for monte carlo algorithm

--- src/Algorithms/MonteCarlo/MonteCarlo.hpp
+++ src/Algorithms/MonteCarlo/MonteCarlo.hpp
@@ -6,3 +6,28 @@
 */
 
 #pragma once
+#include <Map.hpp>
+
+class MonteCarlo {
+
+    public:
+        /**
+         * @brief Construct a new Monte Carlo object.
+         *
+         */
+        MonteCarlo();
+
+        /**
+         * @brief Destroy the Monte Carlo object.
+         *
+         */
+        ~MonteCarlo();
+
+        /**
+         * @brief Compute the best move to play.
+         *
+         * @param map Map to play.
+         * @return std::pair<int, int> Best move to play.
+         */
+        std::pair<int, int> compute(Map map);
+};
