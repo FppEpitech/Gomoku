a842b1cabd349fa0fb2d11b1101bd0048deb28b4
Fradet <axel.fradet@epitech.eu>
2024-11-18 17:48:03 +0100

    tests(AI): Add tests file about multiple line winning line of three

---
+++ tests/complexPattern/test_multipleLineThree.cpp
@@ -0,0 +1,228 @@
+/*
+** EPITECH PROJECT, 2024
+** Gomoku
+** File description:
+** test easy win
+*/
+
+#include <criterion/criterion.h>
+#include <criterion/redirect.h>
+
+#include <string>
+
+#include "../../src/Map/Map.hpp"
+#include "../../src/Parser/Parser.hpp"
+#include "../../src/GameRules/GameRules.hpp"
+
+void redirect_all_std_multiple_line_four(void) {
+    cr_redirect_stdout();
+    cr_redirect_stderr();
+}
+
+std::vector<std::vector<int>> BOARD_LINE_MULTIPLE_THREE_1 = {
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+};
+std::string BOARD_LINE_MULTIPLE_THREE_ANSWER_1 = "11,10\n";
+
+std::vector<std::vector<int>> BOARD_LINE_MULTIPLE_THREE_2 = {
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+};
+std::string BOARD_LINE_MULTIPLE_THREE_ANSWER_2 = "11,10\n";
+
+std::vector<std::vector<int>> BOARD_LINE_MULTIPLE_THREE_3 = {
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+};
+std::string BOARD_LINE_MULTIPLE_THREE_ANSWER_3 = "11,10\n";
+
+std::vector<std::vector<int>> BOARD_LINE_MULTIPLE_THREE_4 = {
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+};
+std::string BOARD_LINE_MULTIPLE_THREE_ANSWER_4 = "11,10\n";
+
+std::vector<std::vector<std::vector<int>>> boards_line_multiple_three = {
+    BOARD_LINE_MULTIPLE_THREE_1,
+    BOARD_LINE_MULTIPLE_THREE_2,
+    BOARD_LINE_MULTIPLE_THREE_3,
+    BOARD_LINE_MULTIPLE_THREE_4
+};
+
+std::vector<std::string> answers_line_multiple_three = {
+    BOARD_LINE_MULTIPLE_THREE_ANSWER_1,
+    BOARD_LINE_MULTIPLE_THREE_ANSWER_2,
+    BOARD_LINE_MULTIPLE_THREE_ANSWER_3,
+    BOARD_LINE_MULTIPLE_THREE_ANSWER_4
+};
+
+void createMapMultipleLineThree(std::string file, std::vector<std::vector<int>> board, int size)
+{
+    FILE *input_file = fopen(file.c_str(), "w+");
+    if (!input_file)
+        cr_assert_fail("Could not open file for writing");
+
+    for (int y = 0; y < size; ++y) {
+        for (int x = 0; x < size; ++x) {
+            if (board[y][x] != 0) {
+                fprintf(input_file, "%d,%d,%d\n", y, x, board[y][x]);
+            }
+        }
+    }
+    fprintf(input_file, "DONE\n");
+    fflush(input_file);
+    fclose(input_file);
+}
+
+Test(LineMultipleThree, test_complex_line_multiple_three1, .timeout = 5, .init = redirect_all_std_multiple_line_four)
+{
+    Parser parser;
+    GameRules gameRules;
+    int i = 0;
+
+    Map map;
+    int size = boards_line_multiple_three[i].size();
+    map.createMap(size);
+    std::string path = "test_line_multiple_three.txt";
+    createMapMultipleLineThree(path, boards_line_multiple_three[i], size);
+    FILE *input_redirected = freopen(path.c_str(), "r", stdin);
+    if (!input_redirected)
+        cr_assert_fail("Failed to redirect stdin to file");
+    parser.parseCommand("BOARD", map, gameRules);
+    fclose(input_redirected);
+    remove(path.c_str());
+    cr_assert_stdout_eq_str(answers_line_multiple_three[i].c_str());
+}
+
+Test(LineMultipleThree, test_complex_line_multiple_three2, .timeout = 5, .init = redirect_all_std_multiple_line_four)
+{
+    Parser parser;
+    GameRules gameRules;
+    int i = 1;
+
+    Map map;
+    int size = boards_line_multiple_three[i].size();
+    map.createMap(size);
+    std::string path = "test_line_multiple_three1.txt";
+    createMapMultipleLineThree(path, boards_line_multiple_three[i], size);
+    FILE *input_redirected = freopen(path.c_str(), "r", stdin);
+    if (!input_redirected)
+        cr_assert_fail("Failed to redirect stdin to file");
+    parser.parseCommand("BOARD", map, gameRules);
+    fclose(input_redirected);
+    remove(path.c_str());
+    cr_assert_stdout_eq_str(answers_line_multiple_three[i].c_str());
+}
+
+Test(LineMultipleThree, test_complex_line_multiple_three3, .timeout = 5, .init = redirect_all_std_multiple_line_four)
+{
+    Parser parser;
+    GameRules gameRules;
+    int i = 2;
+
+    Map map;
+    int size = boards_line_multiple_three[i].size();
+    map.createMap(size);
+    std::string path = "test_line_multiple_three2.txt";
+    createMapMultipleLineThree(path, boards_line_multiple_three[i], size);
+    FILE *input_redirected = freopen(path.c_str(), "r", stdin);
+    if (!input_redirected)
+        cr_assert_fail("Failed to redirect stdin to file");
+    parser.parseCommand("BOARD", map, gameRules);
+    fclose(input_redirected);
+    remove(path.c_str());
+    cr_assert_stdout_eq_str(answers_line_multiple_three[i].c_str());
+}
+
+Test(LineMultipleThree, test_complex_line_multiple_three4, .timeout = 5, .init = redirect_all_std_multiple_line_four)
+{
+    Parser parser;
+    GameRules gameRules;
+    int i = 3;
+
+    Map map;
+    int size = boards_line_multiple_three[i].size();
+    map.createMap(size);
+    std::string path = "test_line_multiple_three3.txt";
+    createMapMultipleLineThree(path, boards_line_multiple_three[i], size);
+    FILE *input_redirected = freopen(path.c_str(), "r", stdin);
+    if (!input_redirected)
+        cr_assert_fail("Failed to redirect stdin to file");
+    parser.parseCommand("BOARD", map, gameRules);
+    fclose(input_redirected);
+    remove(path.c_str());
+    cr_assert_stdout_eq_str(answers_line_multiple_three[i].c_str());
+}
